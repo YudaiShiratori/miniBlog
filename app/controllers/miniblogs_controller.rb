b73070cc4cfcd1148a8746a54397e8bb1e833bae
YudaiShihratori <toritorikebabu1023x@gmail.com>
2018-04-13 06:43:35 +0000

    edit update destroyアクション

--- app/controllers/miniblogs_controller.rb
+++ app/controllers/miniblogs_controller.rb
@@ -1,2 +1,58 @@
 class MiniblogsController < ApplicationController
+   before_action :blog_param, only [:show, :edit, :update, :destroy]
+
+   def index
+     @miniblogs = miniblog.all
+   end
+
+   def new
+     @miniblog = miniblog.new
+   end
+
+   def create
+     miniblog.create(blog_param)
+
+     if @miniblog.save
+       redirect_to miniblogs_path, notice:"投稿しました！"
+     else
+       render 'new'
+     end
+
+   end
+
+   def show
+     @miniblog = miniblog.find(params[:id])
+   end
+
+   def edit
+
+   end
+
+   def update
+     if @miniblog.save
+       redirect_to miniblogs_path, notice:"更新しました！"
+     else
+       render 'new'
+     end
+   end
+
+   def destroy
+       @miniblog.destroy
+       redirect_to miniblogs_path, notice:"削除しました！"
+   end
+
+   private
+
+   def set_blog
+     @miniblog = miniblog.find(params[:id])
+   end
+
+   def blog_params
+     params.require(:miniblog).permit(:name,:text)
+   end
+
+
+
+
+
 end
